2932935b85794ace71ffecc1a57eaccf47763ef5
Andrew <anolte512@gmail.com>
2021-05-18 00:24:09 -0500

    updated resume

--- update.py
+++ update.py
@@ -1,2 +1,3 @@
 from os import system
-system('cp /mnt/c/Users/anolt/Documents/Academic/AndrewNolteResume.pdf ./')
+# system('cp /mnt/c/Users/anolt/Documents/Academic/AndrewNolteResume.pdf ./')
+system('cp ~/Documents/AndrewNolteResume.pdf ./')
